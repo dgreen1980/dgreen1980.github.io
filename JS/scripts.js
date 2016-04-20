bc9a6d5c957d6595f3e9de4bb87ef4ab06e033c9
dgreen1980 <dgreenlawoffice@gmail.com>
2016-04-20 11:26:13 -0700

    changes to pages
    made some changes since learning bootstrap and we started learning JS.

---
+++ JS/scripts.js
@@ -0,0 +1,20 @@
+//data types
+//number
+//string
+//boolean
+
+//boolean operators
+//console.log(5 > 3);
+//comparison operator
+//console.log(5 === 5);
+//console.log(5 >= 5);
+//console.log(2 <= 2);
+//console.log(5 != 5);
+//logical AND
+//&&
+//logical OR
+//||
+//if use confirm instead of prompt it responds in boolean data type when click ok get true if cancel get false
+//symbol for not
+//!
+//In order to write a conditional statement that always runs it should always be true.
